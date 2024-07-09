1a568e5371a8058e0112ca171728ff2a8926476c
Harshal Sahgel <129950309+HarshalSahgel@users.noreply.github.com>
2024-07-09 13:55:49 +0000

    input

--- src/components/App.js
+++ src/components/App.js
@@ -3,9 +3,16 @@ import React from "react";
 import './../styles/App.css';
 
 const App = () => {
+
+  const changingState = (e) =>{
+    let value = e.target.value;
+    <p>Hello {value}!</p>
+  }
+
   return (
     <div>
-        {/* Do not remove the main div */}
+       <input type="text" onChange = {changingState}></input>
+       
     </div>
   )
 }
